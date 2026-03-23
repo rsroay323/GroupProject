dc8d15cc0ae8efbc8bd838820586837272cf54e0
ZainUrfan <z.urfan@Wlv.ac.uk>
2026-03-23 20:13:09 +0000

    Add files via upload
    "Add api.js file to connect frontend to backend"

---
+++ frontend/api.js
@@ -0,0 +1,49 @@
+const BASE_URL = "http://localhost:8000";
+const TOKEN = "securetoken";
+
+// Public endpoints
+async function getFacilities() {
+  const res = await fetch(`${BASE_URL}/facilities`);
+  return res.json();
+}
+
+async function getTransport() {
+  const res = await fetch(`${BASE_URL}/transport`);
+  return res.json();
+}
+
+// Protected endpoints
+async function getClubs() {
+  const res = await fetch(`${BASE_URL}/clubs`, {
+    headers: {
+      "Authorization": `Bearer ${TOKEN}`
+    }
+  });
+  return res.json();
+}
+
+async function getEvents() {
+  const res = await fetch(`${BASE_URL}/events`, {
+    headers: {
+      "Authorization": `Bearer ${TOKEN}`
+    }
+  });
+  return res.json();
+}
+
+async function getSecureData() {
+  const res = await fetch(`${BASE_URL}/secure-data`, {
+    headers: {
+      "Authorization": `Bearer ${TOKEN}`
+    }
+  });
+  return res.json();
+}
+
+window.api = {
+  getFacilities,
+  getTransport,
+  getClubs,
+  getEvents,
+  getSecureData
+};
