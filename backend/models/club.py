b9bf35717e1481ecff9dc9253879a2c07baddd1b
rsroay323 <r.sroay@wlv.ac.uk>
2026-04-06 13:55:39 +0100

    Updated the club model
    included description, location, session date and time
    
    Co-Authored-By: Tanvir6129 <260451861+Tanvir6129@users.noreply.github.com>
    Co-Authored-By: sharadyadav98175 <260629758+sharadyadav98175@users.noreply.github.com>

--- backend/models/club.py
+++ backend/models/club.py
@@ -1,8 +1,13 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Date, Time
 from database import Base
+from datetime import date, time
 
 class Club(Base):
     __tablename__ = "clubs"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(100), nullable=False)
+    desciption = Column(String(255), nullable=False)
+    location = Column(String(100), nullable=False)
+    session_date = Column(Date, nullable=False)
+    session_time = Column(Time, nullable=False)
