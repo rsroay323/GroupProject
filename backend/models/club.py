dd32114274cefd9afd1764af57bafed7a9b5669d
rsroay323 <r.sroay@wlv.ac.uk>
2026-04-07 13:29:04 +0100

    small error fix

--- backend/models/club.py
+++ backend/models/club.py
@@ -7,7 +7,7 @@ class Club(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(100), nullable=False)
-    desciption = Column(String(255), nullable=False)
+    description = Column(String(255), nullable=False)
     location = Column(String(100), nullable=False)
     session_date = Column(Date, nullable=False)
     session_time = Column(Time, nullable=False)
