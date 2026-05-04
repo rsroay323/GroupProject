b2c051cde76b929b76ffc274803659fc4d18ad56
rsroay323 <r.sroay@wlv.ac.uk>
2026-05-04 19:59:32 +0100

    Created the login page
    Co-Authored-By: sarkar340 <260939484+sarkar340@users.noreply.github.com>

--- frontend/api.js
+++ frontend/api.js
@@ -27,6 +27,18 @@ async function getClubs() {
   return res.json();
 }
 
+async function registerUser(data) {
+  const res = await fetch(`${BASE_URL}/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  });
+
+  return res.json();
+}
+
 async function addClub(data) {
   const res = await fetch(`${BASE_URL}/clubs`, {
     method: "POST",
@@ -88,6 +100,7 @@ window.api = {
   getSecureData,
   addClub,
   updateClub,
-  deleteClub
+  deleteClub,
+  registerUser
 
 };
